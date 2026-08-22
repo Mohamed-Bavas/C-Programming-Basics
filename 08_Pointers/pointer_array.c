6c1d11fe88c9bd2c218f797af9bb755a6c082677
Mohamed-Bavas <mohamedbavasmohamedbavas@gmail.com>
2026-08-22 19:37:38 +0530

    added

---
+++ 08_Pointers/pointer_array.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+void main()
+{
+    int arr[5] = {10, 20, 30, 40, 50};
+    int *ptr = arr;
+    int i;
+    for (i = 0; i < 5; i++)
+    {
+        printf("%d ", *(ptr + i));
+    }
+}
