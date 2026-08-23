ddab69514f1136fa76ea90b7c8af93f7997370bd
Mohamed-Bavas <mohamedbavasmohamedbavas@gmail.com>
2026-08-23 18:48:29 +0530

    added

---
+++ 09_Structures_Unions/structure.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+struct Student
+{
+    int roll_no;
+    char name[30];
+    float marks;
+};
+int main()
+{
+    struct Student student = {101, "Bavas", 85.5};
+    printf("Roll No = %d\n", student.roll_no);
+    printf("Name = %s\n", student.name);
+    printf("Marks = %.2f\n", student.marks);
+    return 0;
+}
