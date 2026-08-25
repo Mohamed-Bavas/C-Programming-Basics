30fde59bc61b7a31c537e84de688bd411a5417a1
Mohamed-Bavas <mohamedbavasmohamedbavas@gmail.com>
2026-08-25 21:34:51 +0530

    added

---
+++ 10_Dynamic_Memory/malloc.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+    int *ptr;
+    int i;
+    ptr = (int *)malloc(5 * sizeof(int));
+    if (ptr == NULL)
+    {
+        printf("Memory allocation failed\n");
+        return 1;
+    }
+    for (i = 0; i < 5; i++)
+    {
+        ptr[i] = (i + 1) * 10;
+    }
+    for (i = 0; i < 5; i++)
+    {
+        printf("%d ", ptr[i]);
+    }
+    free(ptr);
+}
