6c04340a76b8380d7f6788098be19d19ab161ecb
Mohamed-Bavas <mohamedbavasmohamedbavas@gmail.com>
2026-08-25 21:38:27 +0530

    added

---
+++ 10_Dynamic_Memory/calloc.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+    int *ptr;
+    int i;
+    ptr = (int *)calloc(5, sizeof(int));
+    if (ptr == NULL)
+    {
+        printf("Memory allocation failed\n");
+        return 1;
+    }
+    for (i = 0; i < 5; i++)
+    {
+        printf("%d ", ptr[i]);
+    }
+    free(ptr);
+}
